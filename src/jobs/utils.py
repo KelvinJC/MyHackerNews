2ffea87ed2a60816a7c8076a34fc504ca44c2d68
Kelvin JC <ojkelvinchu@gmail.com>
2022-10-20 19:37:54 +0100

    refactor: Create separate methods for request and concurrent request.
    Singular responsibilty of class methods within API request class

--- src/jobs/utils.py
+++ src/jobs/utils.py
@@ -1,5 +1,5 @@
 import requests
-import ast
+import json
 from concurrent.futures import ThreadPoolExecutor
 from abc import ABC, abstractmethod
 
@@ -11,24 +11,29 @@ class APIRequest(ABC):
         pass
 
 class HackerAPIJobsRequest(APIRequest):
-    id_url = "https://hacker-news.firebaseio.com/v0/jobstories.json"
-    each_job_story_url = "https://hacker-news.firebaseio.com/v0/item/{}.json"
+    __id_url = "https://hacker-news.firebaseio.com/v0/jobstories.json"
+    __each_job_story_url = "https://hacker-news.firebaseio.com/v0/item/{}.json"
 
+    def __request(self, url):
+        try:
+            r = requests.request("GET", url, timeout=120) 
+            r_text = json.loads(r.text)
+        except ConnectionError as e:  
+            print(e)
+        return r_text
+ 
     def fetch_all_job_post_ids(self):
-        payload = "{}"
-        response = requests.request("GET", self.id_url, data=payload) # should be wrapped in try catch block against network errors
-
-        res_text = response.text
-        # reponse.text appears to be a list of integers cast in a string
-        all_ids = (res_text).strip('[').strip(']').split(',')
-        all_ids_int = [int(i) for i in all_ids]
-        return all_ids_int
-
+        ids = self.__request(self.__id_url)
+        if ids:
+            ids_int = [int(i) for i in ids]
+            return ids_int
+        return None
+     
     def find_new_ids(self):
         '''
-        Queries db to get all past ids
-        Compares list of past ids with current request
-        Returns all new ids or None
+        Query db to get all past ids
+        Compare list of past ids with current request
+        Return all new ids or None
         '''
         request_ids = self.fetch_all_job_post_ids()
         print(request_ids)
@@ -43,42 +48,75 @@ class HackerAPIJobsRequest(APIRequest):
             return new_ids
         return None
 
+    def __concurrent_request(self, urls: list):
+        '''
+        Use multi-threading to make concurrent HTTP requests to a list of urls
+        '''     
+        with ThreadPoolExecutor(max_workers=50) as pool:
+            a = list(pool.map(self.__request, urls))  
+        return a
+
     # Use the obtained ids to fetch each job from the Get item endpoint 
-    def fetch_job_posts_with_id(self, ids):
-        ''' uses multi-threading to make concurrent HTTP requests to external api'''
-        def get_url(url):
-            return requests.get(url).text
-
+    def fetch_job_posts_with_id(self):
+        ''' 
+        Use the obtained ids to fetch each story from the Get item endpoint.
+        '''
+        ids = self.find_new_ids()
         if ids:
-            list_of_urls = [self.each_job_story_url.format(i) for i in ids]
-            
-            with ThreadPoolExecutor(max_workers=50) as pool:
-                a = list(pool.map(get_url, list_of_urls))  
-
-            return a
-       
+            list_of_urls = [self.__each_job_story_url.format(i) for i in ids]
+            response = self.__concurrent_request(list_of_urls)
+            return response
         print('no new job stories from fetch fxn')
         return None
 
-class KeyFinder():
+
+class TypeIsJobChecker:
     def __init__(self, request: APIRequest): 
         self.request = request
+    
+    def get_only_job_stories(self):
+        ne = self.request.fetch_job_posts_with_id()
+        if ne:
+            only_news = [i for i in ne if i.get('type') == 'job']
+            return only_news
+        else:
+            return None
+
+
+class PostHasLinkChecker:
+    def __init__(self, checker: TypeIsJobChecker): 
+        self.checker = checker
+
+    def get_job_posts_with_links(self):
+        nw = self.checker.get_only_job_stories()
+        if nw:
+            only_posts_with_links = [i for i in nw if i.get('url') != '' and i.get('url') != None]
+            return only_posts_with_links
+        return None
+
+
+class KeyFinder:
+    def __init__(self, checker: PostHasLinkChecker): 
+        self.checker = checker
 
     def get_job_posts_with_select_keys(self, keys):
-        new_job_stories = self.request.fetch_job_posts_with_id(self.request.find_new_ids()) 
-        if new_job_stories:
+        ns = self.checker.get_job_posts_with_links()
+        if ns:
             b = []
-            for i in new_job_stories:
-                i = ast.literal_eval(i)
+            for i in ns:
                 choice_dict = {k: v for k, v in i.items() if k in keys }
                 b.append(choice_dict)   
             return b
         else:
-            print('No new job stories')
+            print('No new job posts')
             return None
 
-request = HackerAPIJobsRequest()
-keyfinder = KeyFinder(request)
+   
+request      = HackerAPIJobsRequest()
+type_checker = TypeIsJobChecker(request)
+link_checker = PostHasLinkChecker(type_checker)
+keyfinder    = KeyFinder(link_checker)
+
 
 
 
@@ -108,3 +146,9 @@ def get_page_indices(page, paginator):
 
 
 
+if  __name__ == '__main__':
+
+    choice = ["by", "id", "type", "title", "url", "time"]
+    keys = keyfinder.get_job_posts_with_select_keys(choice)
+    
+    print(keys) 
